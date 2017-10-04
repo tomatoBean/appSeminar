02e966624ac947a39dbc75ac91dfb22880cc5c4c
tomatoBean <fengxianlicloud.com@tomatoBeandeMacBook-Pro.local>
2017-10-05 01:43:44 +0800

    add another sample code for customizable menu

---
+++ MyApplicationMenuDefinition/app/src/main/java/com/example/fengxianlicloudcom/myapplicationmenudefinition/MainActivity.java
@@ -0,0 +1,47 @@
+package com.example.fengxianlicloudcom.myapplicationmenudefinition;
+
+import android.support.design.internal.BottomNavigationItemView;
+import android.support.design.internal.BottomNavigationMenuView;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import java.lang.reflect.Field;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        disableShiftMode(navigation);
+
+    }
+
+
+    // to disable sliding effects
+    public static void disableShiftMode(BottomNavigationView view) {
+        BottomNavigationMenuView menuView = (
+                BottomNavigationMenuView) view.getChildAt(0);
+        try {
+            Field shiftingMode = menuView.getClass().getDeclaredField("mShiftingMode");
+            shiftingMode.setAccessible(true);
+            shiftingMode.setBoolean(menuView, false);
+            shiftingMode.setAccessible(false);
+            for (int i = 0; i < menuView.getChildCount(); i++) {
+                BottomNavigationItemView item = (BottomNavigationItemView) menuView.getChildAt(i);
+                item.setShiftingMode(false);
+                item.setChecked(item.getItemData().isChecked());
+            }
+        } catch (NoSuchFieldException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+}
